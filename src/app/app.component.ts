df1187412513764f03496ef9a54d330e6bf119de
Paige Ruppel <paige.ruppel@gmail.com>
2018-02-09 11:44:33 -0500

    completed directives assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+      .overFiveClicks {
+        background: blue;
+        color: white
+      }
+  `]
 })
 export class AppComponent {
   name = '';
+  buttonClicked = false;
+  numberClicks = 0;
+
+  buttonClicks = [];
+  
+  onButtonClick() {
+    this.buttonClicks.push(new Date());
+    this.buttonClicked = !this.buttonClicked;
+  }
 }
